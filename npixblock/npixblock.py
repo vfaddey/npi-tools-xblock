31bee9ac96e61f28b280ed49bb44fee197dd64a0
vfaddey <vfaddey@yandex.ru>
2025-03-31 11:52:25 +0300

    update

--- npixblock/npixblock.py
+++ npixblock/npixblock.py
@@ -4,7 +4,7 @@ from importlib.resources import files
 
 from web_fragments.fragment import Fragment
 from xblock.core import XBlock
-from xblock.fields import Integer, Scope
+from xblock.fields import Integer, Scope, String
 
 
 class NpiToolsXBlock(XBlock):
@@ -16,9 +16,18 @@ class NpiToolsXBlock(XBlock):
     # self.<fieldname>.
 
     # TO-DO: delete count, and define your own fields.
-    count = Integer(
-        default=0, scope=Scope.user_state,
-        help="A simple counter, to show something happening",
+
+    link_url = String(
+        default="https://example.com",
+        scope=Scope.settings,
+        help="URL, на который будет вести кнопка",
+    )
+
+    # Поле для хранения текста кнопки
+    button_text = String(
+        default="Перейти по ссылке",
+        scope=Scope.settings,
+        help="Текст, отображаемый на кнопке",
     )
 
     def resource_string(self, path):
@@ -28,43 +37,60 @@ class NpiToolsXBlock(XBlock):
     # TO-DO: change this view to display your data your own way.
     def student_view(self, context=None):
         """
-        The primary view of the NpiToolsXBlock, shown to students
-        when viewing courses.
+        Основное представление для студента.
         """
-        html = self.resource_string("static/html/npixblock.html")
-        frag = Fragment(html.format(self=self))
+        print(context)
+        html = self.render_template("static/html/npixblock.html", {
+            "link_url": self.link_url,
+            "button_text": self.button_text,
+        })
+
+        frag = Fragment(html)
         frag.add_css(self.resource_string("static/css/npixblock.css"))
         frag.add_javascript(self.resource_string("static/js/src/npixblock.js"))
-        frag.initialize_js('NpiToolsXBlock')
+        frag.initialize_js('NpixBlock')
         return frag
 
+    def studio_view(self, context=None):
+        """
+        Представление для редактирования в Studio.
+        """
+        html = self.render_template("static/html/npixblock.html", {
+            "link_url": self.link_url,
+            "button_text": self.button_text,
+        })
+
+        frag = Fragment(html)
+        frag.add_css(self.resource_string("static/css/npixblock.css"))
+        frag.add_javascript(self.resource_string("static/js/npixblock.js"))
+        frag.initialize_js('NpixBlock', {
+            "link_url": self.link_url,
+            "button_text": self.button_text,
+        })
+        return frag
     # TO-DO: change this handler to perform your own actions.  You may need more
     # than one handler, or you may not need any handlers at all.
     @XBlock.json_handler
-    def increment_count(self, data, suffix=''):
+    def save_settings(self, data, suffix=''):
         """
-        An example handler, which increments the data.
+        Обработчик для сохранения настроек из Studio.
         """
-        # Just to show data coming in...
-        assert data['hello'] == 'world'
-
-        self.count += 1
-        return {"count": self.count}
+        self.link_url = data.get('link_url', self.link_url)
+        self.button_text = data.get('button_text', self.button_text)
+        return {'result': 'success'}
 
     # TO-DO: change this to create the scenarios you'd like to see in the
     # workbench while developing your XBlock.
+    def render_template(self, path, context):
+        """Утилита для рендеринга HTML-шаблонов."""
+        html = self.resource_string(path)
+        return html.format(**context)
+
     @staticmethod
     def workbench_scenarios():
-        """A canned scenario for display in the workbench."""
+        """Сценарии для тестирования в Workbench."""
         return [
-            ("NpiToolsXBlock",
+            ("NpixBlock",
              """<npixblock/>
-             """),
-            ("Multiple NpiToolsXBlock",
-             """<vertical_demo>
-                <npixblock/>
-                <npixblock/>
-                <npixblock/>
-                </vertical_demo>
              """),
         ]
--- npixblock/static/js/src/npixblock.js
+++ npixblock/static/js/src/npixblock.js
@@ -1,22 +1,52 @@
-/* Javascript for NpiToolsXBlock. */
-function NpiToolsXBlock(runtime, element) {
+function NpixBlock(runtime, element, init_args) {
+    var $element = $(element);
+    var $studentView = $element.find('.npixblock-student-view');
+    var $studioView = $element.find('.npixblock-studio-view');
+    var $buttonTextInput = $element.find('.npixblock-button-text');
+    var $linkUrlInput = $element.find('.npixblock-link-url');
+    var $saveButton = $element.find('.npixblock-save-button');
 
-    function updateCount(result) {
-        $('.count', element).text(result.count);
-    }
+    // Проверяем, находимся ли мы в Studio
+    if (runtime.runtime_version && runtime.runtime_version() === 'studio') {
+        $studentView.hide();
+        $studioView.show();
 
-    var handlerUrl = runtime.handlerUrl(element, 'increment_count');
+        // Обработчик нажатия на кнопку "Сохранить"
+        $saveButton.on('click', function() {
+            var newButtonText = $buttonTextInput.val();
+            var newLinkUrl = $linkUrlInput.val();
 
-    $('p', element).click(function(eventObject) {
-        $.ajax({
-            type: "POST",
-            url: handlerUrl,
-            data: JSON.stringify({"hello": "world"}),
-            success: updateCount
+            // Отправляем данные на сервер через runtime.handlerUrl
+            $.ajax({
+                type: 'POST',
+                url: runtime.handlerUrl(element, 'save_settings'),
+                data: JSON.stringify({
+                    button_text: newButtonText,
+                    link_url: newLinkUrl
+                }),
+                contentType: 'application/json',
+                success: function(response) {
+                    if (response.result === 'success') {
+                        alert('Настройки успешно сохранены!');
+                        // Обновляем отображение кнопки в Studio
+                        $element.find('.npixblock-button').text(newButtonText);
+                        $element.find('a').attr('href', newLinkUrl);
+                    } else {
+                        alert('Ошибка при сохранении настроек.');
+                    }
+                },
+                error: function() {
+                    alert('Произошла ошибка при сохранении.');
+                }
+            });
         });
-    });
+    } else {
+        // Для студента показываем только кнопку
+        $studentView.show();
+        $studioView.hide();
 
-    $(function ($) {
-        /* Here's where you'd do things on page load. */
-    });
+        $element.find('.npixblock-button').on('click', function() {
+            console.log("Кнопка нажата!");
+        });
+    }
 }
